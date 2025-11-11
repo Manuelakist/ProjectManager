54125921a5c85579ea904da2cbef7b3b739ac04b
Manuelakist <manuelaskist11@gmail.com>
2025-11-11 20:34:14 -0300

    feat(view): implementa TextualViewFactory e GuiViewFactory

--- src/view/GuiViewFactory.java
+++ src/view/GuiViewFactory.java
@@ -1,4 +1,40 @@
 package view;
 
+import model.Project;
+import model.ProjectManager;
+
+/**
+ * Fábrica Concreta para criar componentes de UI (interface) no modo Gráfico (Swing).
+ *
+ * @author Manuela Skrsypcsak Kist
+ * @version 1.0
+ */
 public class GuiViewFactory implements IViewFactory {
+
+    /**
+     * {@inheritDoc}
+     * <p>
+     * Cria e retorna uma nova instância da tela principal no modo gráfico (Swing).
+     * </p>
+     * @param manager A instância do ProjectManager.
+     * @return Uma instância de {@link GuiMainMenuView}.
+     */
+    @Override
+    public IMainMenuView createMainMenuView(ProjectManager manager) {
+        return new GuiMainMenuView(manager);
+    }
+
+    /**
+     * {@inheritDoc}
+     * <p>
+     * Cria e retorna uma nova instância da tela de projeto no modo gráfico (Swing).
+     * </p>
+     * @param manager A instância do ProjectManager.
+     * @param project O Projeto específico que esta tela irá exibir.
+     * @return Uma instância de {@link GuiProjectView}.
+     */
+    @Override
+    public IProjectView createProjectView(ProjectManager manager, Project project) {
+        return new GuiProjectView(manager, project);
+    }
 }
--- src/view/TextualViewFactory.java
+++ src/view/TextualViewFactory.java
@@ -1,4 +1,40 @@
 package view;
 
+import model.Project;
+import model.ProjectManager;
+
+/**
+ * Fábrica Concreta para criar componentes de UI (interface) no modo Textual (console).
+ *
+ * @author Manuela Skrsypcsak Kist
+ * @version 1.0
+ */
 public class TextualViewFactory implements IViewFactory {
+
+    /**
+     * {@inheritDoc}
+     * <p>
+     * Cria e retorna uma nova instância da tela principal no modo texto.
+     * </p>
+     * @param manager A instância do ProjectManager.
+     * @return Uma instância de {@link TextualMainMenuView}.
+     */
+    @Override
+    public IMainMenuView createMainMenuView(ProjectManager manager) {
+        return new TextualMainMenuView(manager);
+    }
+
+    /**
+     * {@inheritDoc}
+     * <p>
+     * Cria e retorna uma nova instância da tela de projeto no modo texto.
+     * </p>
+     * @param manager A instância do ProjectManager.
+     * @param project O Projeto específico que esta tela irá exibir.
+     * @return Uma instância de {@link TextualProjectView}.
+     */
+    @Override
+    public IProjectView createProjectView(ProjectManager manager, Project project) {
+        return new TextualProjectView(manager, project);
+    }
 }
